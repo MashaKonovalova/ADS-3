78639412ef94b9c9e0bc5f0f74373e3b07d2e291
masha konovalova <75724191+MashaKonovalova@users.noreply.github.com>
2021-06-07 14:41:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,100 +1,122 @@
 // Copyright 2021 NNTU-CS
 #include <string>
 #include "tstack.h"
-
- prior(char ch) {
+int  Prioritet ( char ch) {
     switch (ch) {
-    case '(':
-        return 0;
-    case ')':
-        return 1;
-    case '+':
-        return 2;
-    case '-':
-        return 2;
-    case '*':
-        return 3;
-    case '/':
-        return 3;
-    default:
-        return -1;
+    case  ' ( ' :
+        возврат  0 ;
+    case  ' ) ' :
+        возврат  1 ;
+    case  ' + ' :
+        возврат  2 ;
+    case  ' - ' :
+        возврат  2 ;
+    case  ' * ' :
+        возврат  3 ;
+    case  ' / ' :
+        возврат  3 ;
+    по умолчанию :
+        возврат - 1 ;
     }
 }
 
-std::string infx2pstfx(std::string inf) {
-  // добавьте сюда нужный код
-  return std::string("");
-    std::string pfx;
-    int i = 0;
-    char ch = inf[i];
-    char top = 0;
-    TStack <char> stackChar;
+std :: string infx2pstfx (std :: string inf) {
+  std :: string pstfx;
+    int я = 0 ;
+    char ch = inf [я];
+    char top = 0 ;
+    TStack < char > stackChar;
     while (ch) {
-        int priority;
-        priority = prior(ch);
-
-        if (priority > -1) {
-            if ((priority == 0 || priority > prior(top) ||
-                stackChar.isEmpty()) && ch != ')') {
-                if (stackChar.isEmpty())
-                    top = ch;
-                stackChar.push(ch);
-            } else if (ch == ')') {
-                while (stackChar.get() != '(') {
-                    pfx.push_back(stackChar.get());
-                    pfx.push_back(' ');
-                    stackChar.pop();
+        int pr;
+        пр = приоритет (ч);
+        if (pr> - 1 ) {
+            if ((pr == 0 || pr> priority (top) ||
+                stackChar. isEmpty ()) && ch! = ' ) ' ) {
+                если (stackChar. isEmpty ())
+                    верх = ч;
+                stackChar. толкать (ч);
+            } else  if (ch == ' ) ' ) {
+                while (stackChar. get ()! = ' ( ' ) {
+                    pstfx. push_back (stackChar. получить ());
+                    pstfx. push_back ( '  ' );
+                    stackChar. pop ();
                 }
-                stackChar.pop();
-                if (stackChar.isEmpty())
-                    top = 0;
+                stackChar. pop ();
+                если (stackChar. isEmpty ())
+                    верх = 0 ;
             } else {
-                while (!stackChar.isEmpty() &&
-                       prior(stackChar.get()) >= priority) {
-                    pfx.push_back(stackChar.get());
-                    pfx.push_back(' ');
-                    stackChar.pop();
-                }
-                if (stackChar.isEmpty())
-                    top = ch;
-                stackChar.push(ch);
+                в то время как (! stackChar. isEmpty () &&
+                       Prioritet (stackChar. get ())> = pr) {
+                    pstfx. push_back (stackChar. получить ());
+                    pstfx. push_back ( '  ' );
+                    stackChar. pop ();
+                  }
+                если (stackChar. isEmpty ())
+                    верх = ч;
+                stackChar. толкать (ч);
             }
         } else {
-            pfx.push_back(ch);
-            pfx.push_back(' ');
+            pstfx. push_back (ch);
+            pstfx. push_back ( '  ' );
         }
-
-        ch = inf[++i];
+        ch = inf [++ i];
     }
-    while (!stackChar.isEmpty()) {
-        pfx.push_back(stackChar.get());
-        pfx.push_back(' ');
-        stackChar.pop();
+    while (! stackChar. isEmpty ()) {
+        pstfx. push_back (stackChar. получить ());
+        pstfx. push_back ( '  ' );
+        stackChar. pop ();
     }
-    pfx.erase(pfx.end() - 1, pfx.end());
-    return pfx;
+    pstfx. стереть (pstfx. end () - 1 , pstfx. end ());
+    return pstfx;
+}
+int  вычисление ( операция char , int n1, int n2) {
+    переключатель (операция) {
+    case  ' + ' :
+        вернуть n1 + n2;
+        перерыв ;
+    case  ' - ' :
+        вернуть n1 - n2;
+        перерыв ;
+    case  ' * ' :
+        вернуть n1 * n2;
+        перерыв ;
+    case  ' / ' :
+        вернуть n1 / n2;
+        перерыв ;
+    }
 }
 
-int eval(std::string pst) {
-  // добавьте сюда нужный код
-  return 0;
-    std::string tstr;
-  TStack<int> stack2;
-  for (int i = 0; i < pst.length(); i++) {
-    if (pst[i] >= '0' && pst[i] <= '9') {
-      tstr = pst[i];
-      stack2.push(pst[i] - '0');
-    } else if (pst[i] != ' ') {
-      int second = stack2.get();
-      stack2.pop();
-      int first = stack2.get();
-      stack2.pop();
-      if (pst[i] == '*') stack2.push(first * second);
-      else if (pst[i] == '/') stack2.push(first / second);
-      else if (pst[i] == '+') stack2.push(first + second);
-      else
-        stack2.push(first - second);
+
+int  eval (std :: string pst) {
+ TStack < int > stackInt;
+    int i = 0 , результат = 0 ;
+    char ch = pst [я];
+    while (ch) {
+        if (ch> = ' 0 ' && ch <= ' 9 ' ) {
+            int insertInt = 0 ;
+            int dec = 1 ;
+            while (ch! = '  ' ) {
+                insertInt + = (ch - 48 ) * dec;
+                уб * = 10 ;
+                ch = pst [++ i];
+            }
+            stackInt. push (insertInt);
+        } else {
+            char operation = ch;
+            i ++;
+            int n2 = stackInt. получить ();
+            stackInt. pop ();
+            int n1 = stackInt. получить ();
+            stackInt. pop ();
+            int результат = вычисление (операция, n1, n2);
+            stackInt. push (результат);
+        }
+        если (i <pst. size ())
+            ch = pst [++ i];
+        еще
+            ch = 0 ;
     }
-  }
-  return stack2.get();
+    результат = stackInt. получить ();
+    stackInt. pop ();
+    вернуть результат;
 }
